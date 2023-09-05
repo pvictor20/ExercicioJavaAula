7354e5b29872cc47596cd5ac945ca164f46ca897
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-09-05 10:59:49 -0300

    Modificação exercicio 10

--- Exercicio10.java
+++ Exercicio10.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Exercicio10 {
     public static void main(String[] args) {
-        float misto, salada, sucoL, sucoM, preco;
+        float misto, salada, sucoL, sucoM;
         byte opcoes;
         String resposta;
 
@@ -28,22 +28,27 @@ public class Exercicio10 {
         System.out.println("Digite 1-Misto quente, 2 - Salada Chinesa, 3 - Suco de laranja, 4 - Suco de manga");
         opcoes = sc.nextByte();
 
+        misto = (float) 5.50;
+        salada = (float) 10.20;
+        sucoL = (float) 4.00;
+        sucoM = (float) 3.50;
+
         switch (opcoes) {
             case 1:
                 System.out.println("Você escolheu misto quente.");
-                System.out.println("O valor ficou em R$5,50. Bom apetite!");
+                System.out.println("O valor ficou em R$" + misto + "Bom apetite!");
                 break;
             case 2:
                 System.out.println("Você escolheu salada chinesa.");
-                System.out.println("O valor ficou em R$10,20. Bom apetite!");
+                System.out.println("O valor ficou em R$" + salada + "Bom apetite!");
                 break;
             case 3:
                 System.out.println("Você escolheu suco de laranja.");
-                System.out.println("O valor ficou em R$4,00. Tenha um excelente drink!");
+                System.out.println("O valor ficou em R$" + sucoL + "Tenha um excelente drink!");
                 break;
             case 4:
                 System.out.println("Você escolheu suco de manga.");
-                System.out.println("O valor ficou em R$3,50. Tenha um excelente drink!");
+                System.out.println("O valor ficou em R$" + sucoM + "Tenha um excelente drink!");
                 break;
             default:
                 System.out.println("Número inválido");
